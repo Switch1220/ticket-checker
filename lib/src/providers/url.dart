02e98c4a79b58e2c8e53a087c93a76f5b3c98601
Switch <switchanmd@gmail.com>
2023-05-20 10:19:45 +0900

    fix: Change api url to mkh

--- lib/src/providers/url.dart
+++ lib/src/providers/url.dart
@@ -1,8 +1,7 @@
 import 'package:ticket_checker/src/providers/prefs.dart';
 
 const _key = 'url';
-// const _default = 'api.mkh.mn';
-const _default = 'https://jsonplaceholder.typicode.com/posts';
+const _default = 'https://api.mkh.mn';
 
 String getUrl() => prefs.getString(_key) ?? _default;
 Future<bool> setUrl() => prefs.setString(_key, _default);
